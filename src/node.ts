507ca0b802b7901cbcef3358524f0c152d3f6f13
Adil Hanney <adilhanney@disroot.org>
2026-08-31 14:19:53 +0100

    fix: leftover hardcoded tab height

--- src/node.ts
+++ src/node.ts
@@ -4,7 +4,7 @@ import type { Forest } from './forest.js';
 import type { Entity } from './ecs.js';
 import type { Ext } from './extension.js';
 import type { Rectangle } from './rectangle.js';
-import type { Stack } from './stack.js';
+import { TAB_HEIGHT, type Stack } from './stack.js';
 import { ShellWindow } from './window.js';
 
 /** A node is either a fork a window */
@@ -228,11 +228,11 @@ export class Node {
                 break;
             // Stack
             case 3:
-                const size = ext.dpi * 4;
+                const tab_height = ext.dpi * TAB_HEIGHT;
 
                 this.inner.rect = area.clone();
-                this.inner.rect.y += size * 6;
-                this.inner.rect.height -= size * 6;
+                this.inner.rect.y += tab_height;
+                this.inner.rect.height -= tab_height;
 
                 for (const entity of this.inner.entities) {
                     record(entity, parent, this.inner.rect);
