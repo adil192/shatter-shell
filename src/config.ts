c2791decd159ed45ff13c92ae99815decdfc8c64
Adil Hanney <adilhanney@disroot.org>
2026-09-06 12:20:50 +0100

    feat: float exception for git credential manager popup

--- src/config.ts
+++ src/config.ts
@@ -60,6 +60,7 @@ export const DEFAULT_FLOAT_RULES: Array<FloatRule> = [
     { class: 'zoom' },
     { class: '^.*action=join.*$' },
     { class: 'gjs' },
+    { class: 'git-credential-manager' },
 ];
 
 export interface WindowRule {
