5f9e7bc2975587dcfe41b9e10e4fc52530dd450c
Adil Hanney <adilhanney@disroot.org>
2026-09-12 14:20:49 +0100

    feat: float exception for Xwayland Video Bridge
    A welcome change for anyone who hops between GNOME and KDE

--- src/config.ts
+++ src/config.ts
@@ -61,6 +61,7 @@ export const DEFAULT_FLOAT_RULES: Array<FloatRule> = [
     { class: '^.*action=join.*$' },
     { class: 'gjs' },
     { class: 'git-credential-manager' },
+    { class: 'xwaylandvideobridge' },
 ];
 
 export interface WindowRule {
