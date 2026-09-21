b2c2506740838b04ec02baba0c676429f9936879
Adil Hanney <adilhanney@disroot.org>
2026-09-22 00:58:41 +0100

    ref: remove obsolete keybindings
    Don't exist or are already the default on modern GNOME versions

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -40,12 +40,8 @@ set_keybindings() {
     dconf write ${KEYS_MUTTER_WAYLAND_RESTORE} "@as []"
     # Hide window: disable <Super>h
     dconf write ${KEYS_GNOME_WM}/minimize "@as ['<Super>comma']"
-    # Open the application menu: disable <Super>m
-    dconf write ${KEYS_GNOME_SHELL}/open-application-menu "@as []"
-    # Toggle message tray: disable <Super>m
+    # Toggle message tray: disable <Super>m, retain <Super>v
     dconf write ${KEYS_GNOME_SHELL}/toggle-message-tray "@as ['<Super>v']"
-    # Show the activities overview: disable <Super>s
-    dconf write ${KEYS_GNOME_SHELL}/toggle-overview "@as []"
     # Switch to workspace left: disable <Super>Left
     dconf write ${KEYS_GNOME_WM}/switch-to-workspace-left "@as ['<Primary><Super>Left','<Primary><Super>${left}']"
     # Switch to workspace right: disable <Super>Right
@@ -100,7 +96,7 @@ set_keybindings() {
     # Rotate Video Lock
     dconf write ${KEYS_MEDIA}/rotate-video-lock-static "@as []"
 
-    # Close Window
+    # Close window: changed from <Alt>F4
     dconf write ${KEYS_GNOME_WM}/close "['<Super>q', '<Alt>F4']"
 }
 
